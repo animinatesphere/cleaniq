b7497251dbd429a7e0b10f0f1f3a458a0c3e5f65
Adeyemi Pelumi <adeyemipelumi1980@gmail.com>
2026-05-17 01:22:11 +0100

    fix: Add localhost:8081 to CORS for Expo Web testing

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['https://www.cleaniqservices.com', 'https://cleaniqservices.com', 'http://localhost:5173'],
+  origin: ['https://www.cleaniqservices.com', 'https://cleaniqservices.com', 'http://localhost:5173', 'http://localhost:8081'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
