b16d5ded3337fb0bb364e43b6270279a224aa07f
Adeyemi Pelumi <adeyemipelumi1980@gmail.com>
2026-07-15 13:06:27 +0100

    update on the app

--- server/index.js
+++ server/index.js
@@ -37,7 +37,8 @@ app.use((req, res, next) => {
 
 // Middleware
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // MongoDB Connection
